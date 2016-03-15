15430ed816ed7bebd1e83b9667116de050be2ca7
Randall Lisy <lisyrj@gmail.com>
2016-03-15 21:16:45 +0000

    usernames

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -7,6 +7,8 @@ RSpec.feature "signing up new users" do
     
     click_link "Sign up"
     
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Doe"
     fill_in "Email", with:"user@example.com"
     fill_in "Password", with:"password"
     fill_in "Password confirmation", with:"password"
@@ -14,6 +16,8 @@ RSpec.feature "signing up new users" do
     click_button "Sign up"
     
     expect(page).to have_content("You have signed up successfully.")
+    visit "/"
+    expect(page).to have_content("John Doe")
   end
   
 end
